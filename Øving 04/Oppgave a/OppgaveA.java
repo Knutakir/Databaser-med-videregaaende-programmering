7dc9402d0c72ce2db1fa362d77d3dc77abe87838
Knutakir <knutkirk@hotmail.com>
2016-01-20 16:31:02 +0100

    Opplasting av øving 4
    Opplasting av øving 4

---
+++ Øving 04/Oppgave a/OppgaveA.java
@@ -0,0 +1,50 @@
+import static javax.swing.JOptionPane.*;
+import java.sql.*;
+
+class OppgaveA{
+	public static void main(String[] args) {
+		try {
+			String databasedriver = "com.mysql.jdbc.Driver";
+			Class.forName(databasedriver);
+
+			String databaseNavn = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/knutakir?user=knutakir&password=lTB55QDc";
+			Connection forbindelse = DriverManager.getConnection(databaseNavn);
+
+			Statement setning = forbindelse.createStatement();
+
+			String bokISBN = showInputDialog("Skriv inn isbn: "); //0-02-219233-1
+
+			ResultSet res = setning.executeQuery("SELECT count(*) eks_nr FROM eksemplar WHERE isbn = '" + bokISBN + "';");
+
+			String utskrift = "";
+			boolean eksistererEks = false;
+			while(res.next()){
+				int antEksemplar = res.getInt("eks_nr");
+				if(antEksemplar == 0){
+					showMessageDialog(null, "Det eksisterer ingen ekemplarer av denne boken.");
+				} else {
+					utskrift += "\nAntall eksamplar: " + antEksemplar;
+					eksistererEks = true;
+				}
+			}
+
+			if(eksistererEks){
+				res = setning.executeQuery("SELECT forfatter, tittel FROM boktittel WHERE isbn = '" + bokISBN + "';");//'0-02-219233-1';");
+
+				while(res.next()){
+					String forfatter = res.getString("forfatter");
+					String bokTittel = res.getString("tittel");
+					utskrift = "Forfatter: " + forfatter + "\nBoktittel: " + bokTittel + utskrift;
+				}
+
+				showMessageDialog(null, utskrift);
+			}
+
+			res.close();
+			setning.close();
+			forbindelse.close();
+		} catch (Exception e){
+			showMessageDialog(null, e.getMessage());
+		}
+	}
+}
---
+++ Øving 04/Oppgave b/OppgaveB.java
@@ -0,0 +1,37 @@
+import static javax.swing.JOptionPane.*;
+import java.sql.*;
+
+class OppgaveB {
+	public static void main(String[] args) {
+		try{
+			String databasedriver = "com.mysql.jdbc.Driver";
+			Class.forName(databasedriver);
+
+			String databaseNavn = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/knutakir?user=knutakir&password=lTB55QDc";
+			Connection forbindelse = DriverManager.getConnection(databaseNavn);
+
+			Statement setning = forbindelse.createStatement();
+
+			//0-02-219233-1
+
+			String person = showInputDialog("Skriv inn navnet: ");
+			String isbn = showInputDialog("Skriv inn isbn: ");
+			int eksemplarNummer = Integer.parseInt(showInputDialog("Skriv inn eksemplarnummer: "));
+
+			String sqlSetning = "UPDATE eksemplar SET laant_av = '" + person + "' WHERE isbn = '" + isbn + "' AND eks_nr = " + eksemplarNummer + " AND laant_av is NULL;";
+
+			if(setning.executeUpdate(sqlSetning) == 0){
+				showMessageDialog(null, "Databasen ble ikke oppdatert");
+			} else {
+				showMessageDialog(null, "Databasen ble oppdatert");
+			}
+
+			//UPDATE eksemplar SET laant_av = 'Per Olsen' WHERE isbn = '0-07-241163-5' AND eks_nr = 1 AND laant_av is NULL;
+			setning.close();
+			forbindelse.close();
+		} catch (Exception e){
+			showMessageDialog(null, e.getMessage());
+		}
+
+	}
+}
