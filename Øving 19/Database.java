5aaf1e97f1c51b3fd55dcdea79cfd33fc8d6f1d6
Knut Kirkhorn <knutkirk@hotmail.com>
2016-03-16 14:33:10 +0100

    Opplasting
    Opplasting av øving 19

--- Øving 19/Database.java
+++ Øving 19/Database.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by Knut on 09.03.2016.
@@ -25,31 +22,147 @@ public class Database {
             forbindelse = DriverManager.getConnection(databaseNavn);
             setning = forbindelse.createStatement();
         } catch (ClassNotFoundException classEx) {
-
+            System.out.println(classEx.getMessage());
         } catch (SQLException sqlEx) {
-
+            System.out.println(sqlEx.getMessage());
         } catch (Exception e) {
-
+            System.out.println(e.getMessage());
         }
     }
 
-    private void utforSporring(String sporring) {
-
-    }
-
     public boolean regNyBok(Bok nyBok) {
+        /**
+         * Denne metoden skal registrere en ny tittel og samtidig eksemplar nummer 1 av denne tittelen.
 
-        return false; // returnerer false dersom en bok med samme isbn eksisterer fra før
+         SQL-setninger:
+
+         insert into boktittel(isbn, forfatter, tittel) values(<isbn>, <forfatter>, <tittel>)
+         insert into eksemplar(isbn, eks_nr) values (<isbn>, 1);
+
+         Metoden skal returnere false dersom bok med dette ISBN er registrert fra før. Da skal ingen oppdatering skje.
+         */
+
+
+        if(isbnEksisterer(nyBok.getIsbn())) {
+            return false;
+        } else {
+            try {
+                forbindelse.setAutoCommit(false);
+
+                String insert1 = "INSERT INTO boktittel(isbn, forfatter, tittel) VALUES('" + nyBok.getIsbn() + "','" + nyBok.getForfatter() + "','" + nyBok.getTittel() + "');";
+                String insert2 = "INSERT INTO eksemplar(isbn, eks_nr) VALUES('" + nyBok.getIsbn() + "', 1);";
+
+                if(setning.executeUpdate(insert1) != 0) {
+                    if(setning.executeUpdate(insert2) != 0) {
+                        forbindelse.commit();
+                        return true;
+                    } else {
+                        forbindelse.rollback();
+                        return false;
+                    }
+                } else {
+                    forbindelse.rollback();
+                    return false;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    forbindelse.setAutoCommit(true);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
     }
 
     public int regNyttEksemplar(String isbn) {
+        /**
+         * Denne metoden skal registrere et nytt eksemplar av en tittel som allerede skal være registrert i databasen. Eksemplarnummeret skal være 1 høyere enn hittil største eksemplarnummer. For å være sikret dette, kan ikke sekvenser/autonummerering brukes. (Du har ikke noen garanti for at slike sekvenser er ubrutt). Du må i stedet bruke teknikken som er brukt i metoden registrerNyPerson(), se leksjonen, eventuelt 4.utgave av boka, side 829.
 
-        return 0; // returnerer 0 dersom ingen bøker med samme isbn eksiterer fra før
+         Metoden skal returnere 0 hvis ingen tittel med dette ISBN eksisterer, ellers skal metoden returnere eksemplarnummeret.
+
+         SQL-setning:
+
+         insert into eksemplar(isbn, eks_nr) values (<isbn>, <eks_nr>);
+         */
+        if(isbnEksisterer(isbn)) {
+            int nyttNr = 1;
+            boolean ok = false;
+            //int antForsok = 0;
+            ResultSet res = null;
+            PreparedStatement selectSetning;
+            PreparedStatement insertSetning;
+
+            while(!ok) {
+                String selectSQL = "SELECT MAX(eks_nr) AS maks FROM eksemplar WHERE isbn = '" + isbn + "';";
+                try {
+                    selectSetning = forbindelse.prepareStatement(selectSQL);
+                    res = selectSetning.executeQuery();
+
+                    res.next();
+                    nyttNr = res.getInt("maks") + 1;
+                    String insertSQL = "INSERT INTO eksemplar(isbn, eks_nr) VALUES(?, ?);";
+                    insertSetning = forbindelse.prepareStatement(insertSQL);
+                    insertSetning.setString(1, isbn);
+                    insertSetning.setInt(2, nyttNr);
+                    insertSetning.executeUpdate();
+                    ok = true;
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                return nyttNr;
+            }
+        } else {
+            return 0; //returnerer 0 dersom ingen bøker med samme isbn eksiterer fra før
+        }
+        return 0;
+    }
+
+    private boolean isbnEksisterer(String isbn){
+        try {
+            ResultSet resultSet = setning.executeQuery("SELECT * FROM boktittel WHERE isbn = '" + isbn + "';");
+            boolean eksisterer = false;
+            while(resultSet.next()) {
+                eksisterer = true;
+            }
+            if(eksisterer) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean laanUtEksemplar(String isbn, String navn, int eksNr) {
+        /**
+         * Denne metoden skal registrere at ei bok er utlånt. Kolonnen utlånt for det aktuelle eksemplaret skal settes lik navnet til låneren. Du trenger ikke ta hensyn til om dette feltet allerede er utfylt (vi har ingen metode for å returnere ei bok).
 
-        return false; // returnerer false dersom isbn og/ eller eksempelnummeret ikke eksiterer fra før
+         Metoden skal returnere false dersom ISBN og/eller eksemplarnummer er ugyldig. (Tips: Det er nok å sjekke at update-setningen returnerer 0, det vil si at ingen rader er oppdatert).
+
+         update eksemplar set laant_av = <navn> where isbn = <isbn> and eks_nr = <eks_nr>
+         */
+        String updSetning = "UPDATE eksemplar set laant_av = '" + navn + "' WHERE isbn = '" + isbn + "' AND eks_nr = '" + eksNr + "';";
+        try {
+            if(setning.executeUpdate(updSetning) != 0){
+                //System.out.println("oppdatert");
+                return true;
+            } else {
+                //System.out.println("Ikkje oppdatert");
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        //return false; //returnerer false dersom isbn og/ eller eksempelnummeret ikke eksiterer fra før
     }
 
     public void lukkForbindelse() {
@@ -57,9 +170,23 @@ public class Database {
             setning.close();
             forbindelse.close();
         } catch (SQLException sqlEx) {
-
+            System.out.println("Error");
         } catch (Exception e) {
-
+            System.out.println("Error");
         }
+    }
+
+    //Testing:
+    public static void main(String[] args) {
+        String databaseNavn = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/knutakir?user=knutakir&password=passsord";//"jdbc:mysql://localhost/øving 19";//"jdbc:mysql://mysql.stud.iie.ntnu.no:3306/knutakir?user=knutakir&password=passord";
+        Database databasen = new Database("com.mysql.jdbc.Driver", databaseNavn);
+        //databasen.regNyBok(new Bok("asdas", "asdasd", "asdasd22"));
+        //databasen.regNyBok(new Bok("0-07-241163-5", "asdasd", "asdasd22"));
+        //databasen.laanUtEksemplar("asdasd", "asdasd", 2);
+        //databasen.laanUtEksemplar("0-07-241163-5", "asdasd", 20);
+        //databasen.laanUtEksemplar("0-07-241163-5", "Ingunn Sund", 2);
+        //System.out.println(databasen.regNyttEksemplar("0-596-00123-1"));
+        //  System.out.println(databasen.regNyBok(new Bok("0-596-00123-2", "Test shiiet", "Ingunn Sund")));
+        databasen.lukkForbindelse();
     }
 }
--- Øving 19/TestKlient.java
+++ Øving 19/TestKlient.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 
-import static javax.swing.JOptionPane.showInputDialog;
-import static javax.swing.JOptionPane.showMessageDialog;
-import static javax.swing.JOptionPane.showOptionDialog;
+import static javax.swing.JOptionPane.*;
 
 /**
  * Created by Knut on 09.03.2016.
@@ -19,38 +17,41 @@ public class TestKlient {
         String[] muligheter = {"Registrer ny bok", "Registrer nytt eksempel", "Lån ut eksempel", "Avslutt"};
         int valg = showOptionDialog(null, "Velkommen", "Hva vil du gjøre?", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, muligheter, muligheter[0]);
 
+        //Variabler som brukes flere plasser:
+        String isbn;
+
         while(valg != AVSLUTT) {
             switch(valg) {
                 case REG_BOK:
-                    String isbn = showInputDialog("Skriv inn isbn til boken: ");
+                    isbn = showInputDialog("Skriv inn isbn til boken: ");
                     String forfatter = showInputDialog("Skriv inn navnet til forfatteren: ");
                     String tittel = showInputDialog("Skriv inn tittelen til boken: ");
-                    Bok nyBok = new Bok(isbn, forfatter, tittel);
+                    Bok nyBok = new Bok(isbn, tittel, forfatter);
                     if(databasen.regNyBok(nyBok)) {
                         showMessageDialog(null, "Boken ble registrert");
                     } else {
                         showMessageDialog(null, "Kunne ikke registrere en bok med gitte data.");
                     }
                     break;
-                
+
                 case REG_EKS:
-                    String isbn2 = showInputDialog("Skriv inn isbn til boken: ");
-                    int eksempelNr = databasen.regNyttEksemplar(isbn2);
-                    if(eksempelNr != 0) {
-                        showMessageDialog(null, "Det ble registrert ett nytt eksemplar av denne boken. Eksempelet fikk nummer: " + eksempelNr + ".");
+                    isbn = showInputDialog("Skriv inn isbn til boken: ");
+                    int eksemplarNr = databasen.regNyttEksemplar(isbn);
+                    if(eksemplarNr != 0) {
+                        showMessageDialog(null, "Det ble registrert ett nytt eksemplar av denne boken. Eksempelet fikk nummer: " + eksemplarNr + ".");
                     } else {
                         showMessageDialog(null, "Det eksisterer ingen bøker med gitt isbn-verdi, derfor kunne ingen nye eksempler registreres.");
                     }
                     break;
 
                 case LAAN_BOK:
-                    String isbn3 = showInputDialog("Skriv inn isbn til boken som skal lånes ut: ");
-                    String utlaaner = showInputDialog("Skriv inn navnet til personen som skal låne boken: ");
-                    int eksempelNr2 = Integer.parseInt(showInputDialog("Skriv inn eksempelnummeret på boken du skal låne ut: "));
-                    if(databasen.laanUtEksemplar(isbn3, utlaaner, eksempelNr2)) {
-                        showMessageDialog(null, "Eksempelet ble lånet ut.");
+                    isbn = showInputDialog("Skriv inn isbn til boken: ");
+                    String utlaaner = showInputDialog("Skriv inn navn til personen som skal leie eksemplaret: ");
+                    int eksNr = Integer.parseInt(showInputDialog("Skriv inn eksemplarnummer som skal lånes ut: "));
+                    if(databasen.laanUtEksemplar(isbn, utlaaner, eksNr)) {
+                        showMessageDialog(null, "Det ble lånt ut et eksemplar til " + utlaaner + ".");
                     } else {
-                        showMessageDialog(null, "Eksempelet ble ikke lånet ut.");
+                        showMessageDialog(null, "Det ble ikke lånt ut noe eksemplar");
                     }
                     break;
 
